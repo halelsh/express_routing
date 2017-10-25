17ca99392f4b097fd645b8775484994e9019c272
adielsh <adielvl@gmail.com>
2017-10-25 15:50:33 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-var express = require('express')
-var app = express()
+let express = require('express')
+let app = express()
 
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', function (req, res) {
